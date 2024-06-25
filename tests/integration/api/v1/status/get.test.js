cb33be978d2fcf129fc764b96604f760ec6616cc
carmogui <pexesan@gmail.com>
2024-06-25 20:44:37 -0300

    improve error handling from `database.js`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,8 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
+  expect(response.status).toBe(200);
+
   const responseBody = await response.json();
 
   const { updated_at, dependencies } = responseBody;
@@ -13,6 +15,4 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(version).toEqual("16.0");
   expect(max_connections).toEqual(100);
   expect(opened_connections).toEqual(1);
-
-  expect(response.status).toBe(200);
 });

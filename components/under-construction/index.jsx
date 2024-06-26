848a2a187f636d7e9729e0dfb18b2c6225563055
carmogui <pexesan@gmail.com>
2024-06-25 22:08:56 -0300

    improve under construction page

--- components/under-construction/index.jsx
+++ components/under-construction/index.jsx
@@ -4,19 +4,37 @@ export function UnderConstruction() {
   return (
     <div className={styles.home}>
       <div className={styles.container}>
-        <h1>🌌 sideuniverse</h1>
-        <div className={styles.textContainer}>
-          <p>
-            here is the internet space where we can share our{" "}
-            <strong>side projects</strong>
-          </p>
-          <p>and together improve them with some cool community feedback</p>
+        <div className={styles.titleContainer}>
+          <h1>🌌 sideuniverse</h1>
+
+          <div className={styles.textContainer}>
+            <p>
+              here is going to be the internet space where we can share our{" "}
+              <strong>side projects</strong>
+            </p>
+            <p>and together improve them with some cool community feedback</p>
+          </div>
         </div>
 
-        <span>also, check my personal website 😄</span>
-        <a href="https://www.carmogui.dev/" target="_blank">
-          carmogui.dev
-        </a>
+        <div className={styles.warningWrapper}>
+          <h2>⚠️ currently working on the API ⚠️</h2>
+          <li>
+            <a href="api/v1/status" target="_blank">
+              api/v1/status
+            </a>
+          </li>
+        </div>
+
+        <div className={styles.endContainer}>
+          <span>also, check my personal website 😄</span>
+          <a
+            className={styles.buttonLink}
+            href="https://www.carmogui.dev/"
+            target="_blank"
+          >
+            carmogui.dev
+          </a>
+        </div>
       </div>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
+import "../styles/global.css";
 import { Analytics } from "@vercel/analytics/react";
 
-function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
@@ -8,5 +9,3 @@ function App({ Component, pageProps }) {
     </>
   );
 }
-
-export default App;

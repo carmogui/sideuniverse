54e0fc0de3ae69136b42aa5e88b1ff58d50a9430
carmogui <pexesan@gmail.com>
2024-09-12 22:06:17 -0300

    fix linting problems

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import "../styles/global.css";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>sideuniverse</title>
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
